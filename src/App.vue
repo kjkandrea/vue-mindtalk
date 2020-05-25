7650b6b548764657edc8fe008ff888fc24e5f1dd
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-05-25 11:12:29 +0900

    Revert "quizIndex => index"
    This reverts commit d4f307797286d95bc978c9cc84f15c6be0bbf4b2.

--- src/App.vue
+++ src/App.vue
@@ -14,15 +14,15 @@
           v-bind:stepRange="stepRange"
         />
         <ol v-if="!finish">
-          <li v-for="(quiz, index) in wpdata.acf.quiz_section" v-show="index + 1 === step" :key="quiz.quiz_question">
+          <li v-for="(quiz, quizIndex) in wpdata.acf.quiz_section" v-show="quizIndex + 1 === step" :key="quiz.quiz_question">
               <span class="question">
-                <span>{{index + 1}}.</span>
+                <span>{{quizIndex + 1}}.</span>
                 {{quiz.quiz_question}}
               </span>
               <quiz-question-items 
                 v-on:pickedArrayPush="pickedArrayPush"
                 v-on:nextQuestion="nextQuestion"
-                v-bind:parentIndex="index"
+                v-bind:parentIndex="quizIndex"
                 v-bind:items="quiz.quiz_question_items"
               />
           </li>
