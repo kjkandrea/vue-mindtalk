c85114d2ebaf5b72a9eebdd3f8b1d02ef5ca7a18
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-05-25 19:25:28 +0900

    컴포넌트화 : Quiz Questions
    퀴즈 질문(Questions)을 표시하는 컴포넌트 입니다.

---
+++ src/components/quiz/QuizQuestions.vue
@@ -0,0 +1,33 @@
+<template>
+    <ol>
+      <li v-for="(quiz, index) in items" v-show="index + 1 === step" :key="quiz.quiz_question">
+        <span class="question">
+          <span>{{index + 1}}.</span>
+          {{quiz.quiz_question}}
+        </span>
+        <quiz-question-items 
+          v-bind:parentIndex="index"
+          v-bind:items="quiz.quiz_question_items"
+        />
+      </li>
+  </ol>
+</template>
+    
+<script>
+import QuizQuestionItems from './QuizQuestionItems.vue'
+
+export default {
+  components: {
+    QuizQuestionItems
+  },
+  name: 'QuizQuestions',
+  props: {
+    step: Number,
+    items: Array
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
