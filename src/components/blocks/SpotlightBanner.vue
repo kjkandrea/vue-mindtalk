a4103a0571cd181e6a1af1078918cdd56d136f91
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-05-29 17:32:45 +0900

    ajax요청이 늘어남에따라 서버 url을 전역객체 projectURL로 관리

--- src/components/blocks/SpotlightBanner.vue
+++ src/components/blocks/SpotlightBanner.vue
@@ -37,7 +37,7 @@ export default {
   },
   mounted(){
     // Primary 카테고리로 등록된 Quiz 콘텐츠 json 불러오기
-    fetch(`https://mindtalk.shoplic.store/wp-json/wp/v2/quiz?categories=3&_embed`)
+    fetch(`${window.projectURL}/wp-json/wp/v2/quiz?categories=3&_embed`)
       .then((r) => r.json())
       .then((res) => this.wpdata = res)
   }
